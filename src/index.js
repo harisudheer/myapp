fe76bc19e3fe5f87568225cdaa81fe7c843cb526
npx <harisudheer@lyrostech.com>
2024-03-11 15:21:55 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -9,12 +9,25 @@ import App15 from './App15';
 import App16 from './App16';
 import App13a from './App13a';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   /* <> <div><h1 id='titlee'>Fruits</h1></div>
+// <hr></hr>
+// <div id="products" class="main"><Fruits /></div>
+// </> */
+// {/* <App16 /> */}
+// <>
+// <App13a />
+// </>
+// );
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App13a from "./App13a";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  /* <> <div><h1 id='titlee'>Fruits</h1></div>
-<hr></hr>
-<div id="products" class="main"><Fruits /></div>
-</> */
-{/* <App16 /> */}
-<App13a />
+  <>
+    <App13a />
+  </>
 );
