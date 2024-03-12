19429314a40e619dbba702bcb6c02a61e28b715c
npx <harisudheer@lyrostech.com>
2024-03-12 15:56:08 +0530

    commit

--- src/App15.js
+++ src/App15.js
@@ -1,17 +1,40 @@
-import React from "react";
-import Feeds from "./Feeds";
-import Post from "./Post";
+// import React from "react";
+// import Feeds from "./Feeds";
+// import Post from "./Post";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// export default function App15() {
+//   return (
+//     <div>
+//       <Router>
+//         <Link to="/">Feeds</Link> | <Link to="/post">Post</Link>
+//         <Routes>
+//             <Route path="/" index element={<Feeds/>}></Route>
+//             <Route path="/post" element={<Post/>}></Route>
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Feeds from "./Feeds";
+import Posts from "./Post";
+import Comments from './Comments';
+import Album from './Album';
+
 export default function App15() {
   return (
     <div>
-      <Router>
-        <Link to="/">Feeds</Link> | <Link to="/post">Post</Link>
-        <Routes>
-            <Route path="/" index element={<Feeds/>}></Route>
-            <Route path="/post" element={<Post/>}></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+    <Router>
+      <Link to="/">Feeds</Link> | <Link to="/post">Post</Link> | <Link to="/Album">Album</Link>| <Link to="/Comments">Comments</Link>
+      <Routes>
+          <Route path="/" index element={<Feeds/>}></Route>
+          <Route path="/post" element={<Posts/>}></Route>
+          <Route path="/Album" element={<Album/>}></Route>
+          <Route path="/Comments" element={<Comments/>}></Route>
+      </Routes>
+    </Router>
+  </div>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,33 +1,45 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import './Fruits.css';
+// import Fruits from './Fruits';
+// import App14 from './App14';
+// import App1 from './App1';
+// import App15 from './App15';
+// import App16 from './App16';
+// import App13a from './App13a';
+
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+// // root.render(
+// //   /* <> <div><h1 id='titlee'>Fruits</h1></div>
+// // <hr></hr>
+// // <div id="products" class="main"><Fruits /></div>
+// // </> */
+// // {/* <App16 /> */}
+// // <>
+// // <App13a />
+// // </>
+// // );
+// // import React from "react";
+// // import ReactDOM from "react-dom/client";
+// // import "./index.css";
+// // import App13a from "./App13a";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//     <App13a />
+//   </>
+// );
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './Fruits.css';
-import Fruits from './Fruits';
-import App14 from './App14';
-import App1 from './App1';
-import App15 from './App15';
-import App16 from './App16';
 import App13a from './App13a';
+import App15 from './App15';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   /* <> <div><h1 id='titlee'>Fruits</h1></div>
-// <hr></hr>
-// <div id="products" class="main"><Fruits /></div>
-// </> */
-// {/* <App16 /> */}
-// <>
-// <App13a />
-// </>
-// );
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App13a from "./App13a";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <App13a />
-  </>
+<App15 />
 );
