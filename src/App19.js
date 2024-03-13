17a0d7d1850e0c6b1ef7003747ff175f4677568c
npx <harisudheer@lyrostech.com>
2024-03-13 14:08:08 +0530

    commit

--- src/App19.js
+++ src/App19.js
@@ -15,8 +15,8 @@ export default function App19() {
   return (
     <div>
       <App19child>
-        <h1>Hello World</h1>
-        <p>This is a paragraph</p>
+        <h1>Hello Hari</h1>
+        <p>Concentrate on your code and improve the skills</p>
       </App19child>
     </div>
   );
