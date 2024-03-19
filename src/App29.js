1d5a8e380718269784b497d35f1dcaa04b5c2b61
npx <harisudheer@lyrostech.com>
2024-03-19 14:55:15 +0530

    commit for axios

--- src/App29.js
+++ src/App29.js
@@ -3,7 +3,7 @@ import { useFetch } from "./useFetch";
 export default function App29() {
   const url = "https://jsonplaceholder.typicode.com/users/";
   const [data] = useFetch(url);
-  console.log(data.length)
+  console.log(data.length);
   return (
     <div>
       {data && data.map((elem) => (<li key={elem.id}>{elem.name}</li>))}
